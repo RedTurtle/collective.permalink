f5ab6981fcb22d7690a61b07171c3f4e5c69dde3
keul <luca@keul.it>
2012-12-06 17:04:12 +0000

    Fixed import for Plone 4.2

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1.0'
+version = '0.2.0.dev0'
 
 setup(name='collective.permalink',
       version=version,
-      description="Show a new link in Plone contents. This will not change if you move the content itself.",
+      description="Show a new link (permalink) in Plone contents. "
+                  "This will not change if you move the content itself.",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
@@ -16,8 +17,8 @@ setup(name='collective.permalink',
         ],
       keywords='plone plonegov permalink',
       author='RedTurtle Technology',
-      author_email='sviluppoplone@redturtle.net',
-      url='http://svn.plone.org/svn/collective/collective.permalink',
+      author_email='sviluppoplone@redturtle.it',
+      url='http://plone.org/products/collective.permalink',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
@@ -32,5 +33,4 @@ setup(name='collective.permalink',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      paster_plugins=["ZopeSkel"],
       )
